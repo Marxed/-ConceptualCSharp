addd91c4792b6f5ab66ee7b9bb72a1accd7bf715
Marcus Carvalho <marcus.carvalho@cinbesa.com.br>
2023-02-19 12:46:02 -0300

    Deserialize JSon

---
+++ Advanced/Solution1/DeserializeJSON/DeserializeJSON/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Text.Json;
+using Library;
+
+string jsonString = File.ReadAllText("C:\\CodeStudy\\ConceptualCsharp\\ConceptualCSharp\\Advanced\\Solution1\\files\\serialize.json");
+
+Users? deserialize =  JsonSerializer.Deserialize<Users>(jsonString);
+
+Console.WriteLine(deserialize.Name);
+Console.WriteLine(deserialize.Stand);
+Console.ReadKey();  
+
