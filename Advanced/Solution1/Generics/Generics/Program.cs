54d90bf1ec93585ad2b642fb08242d75ef704eb6
Marcus Carvalho <marcus.carvalho@cinbesa.com.br>
2023-02-21 13:43:53 -0300

    Generics 2

--- Advanced/Solution1/Generics/Generics/Program.cs
+++ Advanced/Solution1/Generics/Generics/Program.cs
@@ -5,14 +5,21 @@ using Generics;
 using Library;
 
 Book book= new Book() {Editor="Micheal", Pages="389", Title="Practical Usage", Writer="Swan" };
-Runner Runner= new Runner() { Age="32", NickName="Wally West", Speed="No mensure", Status="live"};
+Runner runner= new Runner() { Age="32", NickName="Wally West", Speed="No mensure", Status="live"};
 Users Users= new Users() { Name="Joseph Joestar", Stand="Purple Hermit"};
 
 
 Serializer.Serializerr(book);
-Serializer.Serializerr(Runner);
+Serializer.Serializerr(runner);
 Serializer.Serializerr(Users);
 
-Book book2 = Serializer.Deserializer();
-Runner Runner2 = Serializer.Deserializer();
-Users users2 = Serializer.Deserializer();
+Book book2 = Serializer.Deserializer<Book>();
+Runner runner2 = Serializer.Deserializer<Runner>();
+Users users2 = Serializer.Deserializer<Users>();
+
+
+Console.WriteLine("Book "+ book2.Editor);
+Console.WriteLine("Runner "+ runner2.NickName);
+Console.WriteLine("Users "+ users2.Name);
+
+Console.ReadKey();
--- Advanced/Solution1/Generics/Generics/Serializer.cs
+++ Advanced/Solution1/Generics/Generics/Serializer.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Generics
 {
     public class Serializer
     {
-        public static void Serializerr(obj)
+        public static void Serializerr(object obj)
         {
+            string serialize = JsonSerializer.Serialize(obj);
+
+            StreamWriter sw = new StreamWriter(@"C:\CodeStudy\ConceptualCsharp\ConceptualCSharp\Advanced\Solution1\files\"+ obj.GetType().Name +".json");
+
+            sw.Write(serialize);
+            sw.Close();
+
         }
-        public static (Classe) Deserializer()
+        public static T Deserializer<T>()
         { 
         }
     }
